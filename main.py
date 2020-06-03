c6b89391b15be4478649990e0de40203c84e6d30
gaverdugo <7308497+gaverdugo@users.noreply.github.com>
2020-06-03 16:56:58 -0700

    changed id name in states file and added id to cases file

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pandas as pd
 def main():
   csv_df = pd.read_csv('casos-diarios.csv')
   states_info_df = pd.DataFrame(pd.concat([csv_df['cve_ent'], csv_df['poblacion'], csv_df['nombre']], axis=1))
+  states_info_df.columns = ['id', 'population', 'name']
   cases_df = csv_df.drop(csv_df.columns[[0,1,2]], axis=1)
-  states_ids = list(states_info_df['cve_ent'])
+  states_ids = list(states_info_df['id'])
   cases_list = []
 
   for (column_name, column_data) in cases_df.iteritems():
@@ -12,9 +13,10 @@ def main():
     cases_list = cases_list + cases_day
 
   ordered_cases_df = pd.DataFrame(cases_list)
+  ordered_cases_df.index.name = 'id'
 
   states_info_df.to_csv('states.csv', index=False)
-  ordered_cases_df.to_csv('cases.csv', index=False)
+  ordered_cases_df.to_csv('cases.csv')
 
 
 if __name__ == "__main__":
