dec49e4112bdeae787c88202ff520abb772cabc9
gaverdugo <7308497+gaverdugo@users.noreply.github.com>
2020-06-03 18:04:14 -0700

    cases dates are now in YYYY-MM-DD format

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 
 def main():
   csv_df = pd.read_csv('casos-diarios.csv')
@@ -9,7 +10,8 @@ def main():
   cases_list = []
 
   for (column_name, column_data) in cases_df.iteritems():
-    cases_day = [{"state_id": states_ids[i], "cases": column_data[i], "date": column_name} for i in range(0, len(states_ids))]
+    date = datetime.datetime.strptime(column_name, '%d-%m-%Y').strftime('%Y-%m-%d')
+    cases_day = [{"state_id": states_ids[i], "cases": column_data[i], "date": date} for i in range(0, len(states_ids))]
     cases_list = cases_list + cases_day
 
   ordered_cases_df = pd.DataFrame(cases_list)
